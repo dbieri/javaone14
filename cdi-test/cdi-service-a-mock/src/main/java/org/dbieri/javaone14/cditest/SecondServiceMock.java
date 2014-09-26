b19de8d3d38e04e66304a6c86c4e6c81e7f1d2fe
Daniel Bieri <daniel.bieri@anea.ch>
2014-09-26 16:10:25 +0200

    line ending test

--- cdi-test/cdi-service-a-mock/src/main/java/org/dbieri/javaone14/cditest/SecondServiceMock.java
+++ cdi-test/cdi-service-a-mock/src/main/java/org/dbieri/javaone14/cditest/SecondServiceMock.java
@@ -1,13 +1,11 @@
 package org.dbieri.javaone14.cditest;
 
-import org.dbieri.javaone14.cditest.SecondService;
-
 import javax.enterprise.inject.Alternative;
 
 @Alternative
 public class SecondServiceMock extends SecondService {
 	@Override
 	public String passthroughLabel() {
-		return "hello";
-	}
+        return "Hello, World!";
+    }
 }
